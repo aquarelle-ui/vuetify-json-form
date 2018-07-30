30626a365c7f19ed06695cc1c22e1975fd704a7b
sorinsarca <sarca_sorin@hotmail.com>
2018-07-30 16:07:41 +0300

    Updated

--- src/controls/Description/control.vue
+++ src/controls/Description/control.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <h3>{{$intl.translate(display.title)}}</h3>
-        <div>{{$intl.translate(display.text)}}</div>
-    </div>
+    <v-alert :value="true" :outline="outline" :color="color" :icon="icon">
+        <div class="title" v-if="display.title != null" v-text="$intl.translate(display.title)"></div>
+        <div v-if="display.text != null" v-html="$intl.translate(display.text)"></div>
+    </v-alert>
 </template>
 <script>
     import {JsonFormElementMixin} from "@aquarelle/json-form";
@@ -10,5 +10,31 @@
     export default {
         name: 'description-control',
         mixins: [JsonFormElementMixin],
+        computed: {
+            outline() {
+                return this.display.hasOwnProperty('outline') ? Boolean(this.display.outline) : true;
+            },
+            color() {
+                return this.display.color || 'info';
+            },
+            icon() {
+                if (this.display.icon) {
+                    return this.$controlIcon(this.display.icon);
+                }
+
+                switch (this.color) {
+                    case 'success':
+                        return 'check_circle';
+                    case 'info':
+                        return 'info';
+                    case 'warning':
+                        return 'warning';
+                    case 'error':
+                        return 'error';
+                }
+
+                return undefined;
+            }
+        }
     };
 </script>
